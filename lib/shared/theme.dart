4c748831f962e8e939ed067b492322f5da1401be
kickerif20 <kickerif.20@gmail.com>
2021-01-07 23:39:59 +0700

    first commit general page

---
+++ lib/shared/theme.dart
@@ -0,0 +1,6 @@
+part of 'shared.dart';
+
+const double defaultMargin = 10;
+Color mainColor = "FFC700".toColor();
+Color greyColor = "8D92A3".toColor();
+TextStyle greyFontStyle = GoogleFonts.poppins().copyWith(color:greyColor);
---
+++ lib/ui/pages/pages.dart
@@ -0,0 +1,8 @@
+// pages mempunyai bagian di general_page
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:food_market/shared/shared.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:supercharged/supercharged.dart';
+
+part 'general_page.dart';
