7549d7c9af1801ada542a8ae782d10b13ed3986a
kickerif20 <kickerif.20@gmail.com>
2021-01-23 16:50:27 +0700

    Food Detail Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_market/models/models.dart';
 import 'package:get/get.dart';
 import 'ui/pages/pages.dart';
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainPage()
+      home: FoodDetailsPage(transaction: Transaction(food: mockFoods[0])),
     );
   }
 }
--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,4 +1,6 @@
 // pages mempunyai bagian di general_page
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -6,6 +8,7 @@ import 'package:food_market/shared/shared.dart';
 import 'package:food_market/ui/widgets/widgets.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import 'package:supercharged/supercharged.dart';
 import 'package:food_market/models/models.dart';
 
@@ -15,3 +18,4 @@ part 'sign_up_page.dart';
 part 'address_page.dart';
 part 'main_page.dart';
 part 'food_page.dart';
+part 'food_details_page.dart';
